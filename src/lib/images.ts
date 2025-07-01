da0e9484769158a5a112c07814131db971def73c
Christopher Høe <christopher.hoee@gmail.com>
2025-07-01 22:08:24 +0200

    fix: errors and image not rendering

--- src/lib/images.ts
+++ src/lib/images.ts
@@ -1,15 +1,21 @@
 import ompaProjectImage from '../assets/images/ompa_project_image.png';
 import placeholder from '../assets/images/placeholder.webp';
-// import other images
+// ... import other project images
 
 export const projectImages = {
 	'ompa_project_image.png': ompaProjectImage,
 	'placeholder.webp': placeholder
-	// add other images
+	// ... add other image key-value pairs here
 } as const;
 
 export type ImageKey = keyof typeof projectImages;
 
-export function getProjectImage(imageName: string): ImageMetadata {
-	return projectImages[imageName as ImageKey] || placeholder;
+export function getProjectImage(imageName: string | undefined | null) {
+	// return placeholder
+	if (imageName && imageName in projectImages) {
+		return projectImages[imageName as ImageKey];
+	}
+
+	console.warn(`Image "${imageName}" not found. Falling back to placeholder.`);
+	return placeholder;
 }
