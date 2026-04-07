f28d6cd820bd7df508521a9b2eec348adc6b15e9
Christopher Høe <christopher.hoee@gmail.com>
2026-04-07 11:23:21 +0200

    feat: fix format

--- src/utils/seo.ts
+++ src/utils/seo.ts
@@ -1,33 +1,33 @@
 export const seo = ({
-  title,
-  description,
-  image,
-  url,
+	title,
+	description,
+	image,
+	url,
 }: {
-  title: string;
-  description?: string;
-  image?: string;
-  url?: string;
+	title: string;
+	description?: string;
+	image?: string;
+	url?: string;
 }) => {
-  const tags = [
-    { title },
-    ...(description ? [{ name: "description", content: description }] : []),
-    { property: "og:type", content: "website" },
-    { property: "og:title", content: title },
-    ...(description
-      ? [{ property: "og:description", content: description }]
-      : []),
-    ...(url ? [{ property: "og:url", content: url }] : []),
-    ...(image ? [{ property: "og:image", content: image }] : []),
-    {
-      name: "twitter:card",
-      content: image ? "summary_large_image" : "summary",
-    },
-    { name: "twitter:title", content: title },
-    ...(description
-      ? [{ name: "twitter:description", content: description }]
-      : []),
-    ...(image ? [{ name: "twitter:image", content: image }] : []),
-  ];
-  return tags;
+	const tags = [
+		{ title },
+		...(description ? [{ name: "description", content: description }] : []),
+		{ property: "og:type", content: "website" },
+		{ property: "og:title", content: title },
+		...(description
+			? [{ property: "og:description", content: description }]
+			: []),
+		...(url ? [{ property: "og:url", content: url }] : []),
+		...(image ? [{ property: "og:image", content: image }] : []),
+		{
+			name: "twitter:card",
+			content: image ? "summary_large_image" : "summary",
+		},
+		{ name: "twitter:title", content: title },
+		...(description
+			? [{ name: "twitter:description", content: description }]
+			: []),
+		...(image ? [{ name: "twitter:image", content: image }] : []),
+	];
+	return tags;
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,44 @@
+import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
 const config = defineConfig({
 	resolve: {
 		tsconfigPaths: true,
 	},
-	plugins: [devtools(), tailwindcss(), tanstackStart({
-        prerender: {
-            enabled: true,
-            crawlLinks: true,
-            autoStaticPathsDiscovery: true,
-            concurrency: 2,
-            retryCount: 2,
-            retryDelay: 1000,
-            maxRedirects: 5,
-            failOnError: true,
-        },
-        sitemap: {
-            enabled: true,
-            host: "example.com", // fix later
-        },
-    }), viteReact({
-        babel: {
-            plugins: ["babel-plugin-react-compiler"],
-        },
-    }), cloudflare({
-        viteEnvironment: {
-            name: "ssr"
-        }
-    })],
+	plugins: [
+		devtools(),
+		tailwindcss(),
+		tanstackStart({
+			prerender: {
+				enabled: true,
+				crawlLinks: true,
+				autoStaticPathsDiscovery: true,
+				concurrency: 2,
+				retryCount: 2,
+				retryDelay: 1000,
+				maxRedirects: 5,
+				failOnError: true,
+			},
+			sitemap: {
+				enabled: true,
+				host: "example.com", // fix later
+			},
+		}),
+		viteReact({
+			babel: {
+				plugins: ["babel-plugin-react-compiler"],
+			},
+		}),
+		cloudflare({
+			viteEnvironment: {
+				name: "ssr",
+			},
+		}),
+	],
 });
 
 export default config;
